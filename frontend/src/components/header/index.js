5157535eb0a4c745edd087492e0c6cf92ebe3fb8
Ted Johansson <tedjohanssondeveloper@gmail.com>
2019-12-17 19:28:49 +0100

    Style changes to logo display

--- frontend/src/components/header/index.js
+++ frontend/src/components/header/index.js
@@ -4,7 +4,7 @@ import mainLogo from '../../assets/logo.svg'
 
 const Header = () => (
 	<nav className="navbar navbar-expand-lg navbar-dark bg-dark" >
-		<Link className="navbar-brand" href="/"><img src={mainLogo} height="30" alt=""/></Link>
+		<Link className="navbar-brand mb-1" href="/"><img src={mainLogo} height="25" alt=""/></Link>
 		<div id="navbarNav" className="collapse navbar-collapse">
 			<ul className="navbar-nav mr-auto">
 				<li className="nav-item">
